2b98e7dd751dee0d8448d2e9657971e25f02b56d
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2022-07-15 00:04:57 -0400

    Fix up draw service

--- apps/code-server/launch-code-server.sh
+++
@@ -1,5 +0,0 @@
-set -x
-set -e
-
-cd ~ && nohup code-server --bind-addr 0.0.0.0:8081 2>/dev/null &
-
--- apps/code-server/stop-code-server.sh
+++
@@ -1,4 +0,0 @@
-set -x
-set -e
-
-sudo ps aux | grep -G /code-server | grep -v grep | awk '{print $2}' | xargs kill -9
