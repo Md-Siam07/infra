4a568a05c88f25c339c1f909ae9b0303adeba2df
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2022-11-08 19:05:39 -0500

    Update infra

--- docker-images/airflow/k8s/start_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f airflow-env-configmap.yaml
-kubectl apply -f gitsync-env-configmap.yaml
-kubectl apply -f airflow.yaml
--- docker-images/airflow/k8s/stop_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kubectl delete -f airflow-env-configmap.yaml
-kubectl delete -f gitsync-env-configmap.yaml
-kubectl delete deployment airflow-scheduler web
